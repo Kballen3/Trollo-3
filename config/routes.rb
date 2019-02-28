f1166adee2eecb723344e54db8c8a89cffcd995a
Kaleb Allen <7kaleb29allen@gmail.com>
2019-02-28 14:58:58 -0700

    added controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  root "boards#index"
+  
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :boards
+  
+
+
+  resources :boards do
+    resources :lists
+  end
+
+  resources :lists do
+    resources :tasks
+  end
+
 end
